68ac28ab51b2254aae13c5654e7e90d4295830a3
Yokoyama Kameyo (ZIN) <bronze.drone.0f@icloud.com>
2025-04-10 23:34:50 +0700

    feat: :loud_sound: log listening port

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { defaultTo } from 'lodash';
 
 async function bootstrap() {
+  const logger = new Logger
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = defaultTo(process.env.PORT, 3000);
+  await app.listen(port , ()=>{
+    logger.log(`Application is running on: http://localhost:${port}`);
+  });
 }
 bootstrap();
